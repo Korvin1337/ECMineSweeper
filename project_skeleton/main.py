8f08d03f1e8293e079ecc93afb8f82e48e5157ea
Korvin1337 <korvin1337@gmail.com>
2023-12-12 18:32:32 +0100

    minesweeper v1.1.0

--- project_skeleton/main.py
+++ project_skeleton/main.py
@@ -11,6 +11,9 @@ SCREEN_MIN_SIZE = 750  # Can be made to autoadjust after % of ur screen
 amount_of_cells = 16  # The amount of cells is equal in rows and columns, 16x16 (LOCKED)
 bomb_chance = 0.2  # Change to prefered value or use default 0.25
 bomb_counter = 0
+amount_of_bombs = 0
+amount_of_cells_revealed = 0
+cells_in_game = 256
 
 CELL_SIZE = SCREEN_MIN_SIZE // amount_of_cells  # how big can each cell be?
 READJUSTED_SIZE = CELL_SIZE * amount_of_cells
@@ -25,13 +28,16 @@ cells = []
 
 
 def create_cells():
-    y = -46 # Update value for each column, increase by 750 / 16 = 46 (CHANGE TO 0 IF NOT WORKING)
+    global amount_of_bombs
+    y = -46 # Update value for each column, increase by 750 / 16 = 46
     for a_row in range(amount_of_cells):
         row = []
         y += 46
         x = 0
         for a_column in range(amount_of_cells):
-            new_cell = Cell(x, y, CELL_WIDTH, CELL_HEIGHT, bomb_chance)
+            new_cell = Cell(x, y, CELL_WIDTH, CELL_HEIGHT, bomb_chance) # Create the cells by usimg the Cell class constructor
+            if new_cell.bomb:
+                amount_of_bombs = amount_of_bombs + 1 # Keep track of how many bombs are in the playfield
             row.append(new_cell)
             x += 46
         cells.append(row)
@@ -92,17 +98,31 @@ def left_mouse_button_click(position):
                 handle_click(cell)
 
 def reset_game():
+    global bomb_counter, amount_of_cells_revealed, cells, amount_of_bombs
+    bomb_counter = 0
+    amount_of_cells_revealed = 0
+    amount_of_bombs = 0
     for row in cells:
         for cell in row:
             cell.reset()
+    cells.clear()
+    print("--------------------------------------------------------")
+    print("-------------------New Game Starting--------------------")
+    print("--------------------------------------------------------")
+    main()
+
 
 def reveal_bomb(cell):
     global bomb_counter # Keep track of how many times a bomb has been clicked
     bomb_counter += 1
-    print("Boom! You clicked a bomb!")
+    print("B00M! You clicked a bomb!")
     cell.selected = True
     if(bomb_counter > 3):
-        print("You lose!")
+        counter = 0
+        while(counter < 25):
+            print("--------------------------------------------------------")
+            print("-------------------You lose!----------------------------")
+            counter += 1
         reset_game()
     # End game ? Reveal all bombs ?
 
@@ -110,7 +130,7 @@ def reveal_bomb(cell):
 def handle_click(cell):
     print("Clicked on cell:", cell.x, cell.y)
 
-    if cell.bomb:
+    if cell.bomb and not cell.selected:
         reveal_bomb(cell)
     elif not cell.selected:
         cell.selected = True
@@ -118,19 +138,25 @@ def handle_click(cell):
 
 
 
+# Reveals the cell that is clicked, if the cell clicked is a 0 it works together with reveal_neighbours to reveal it´s neighbours (non bombs since they are none around it)
 def reveal_cells_around(cell):
     print("Reveal non-bomb cells around:", cell.x, cell.y)
+    global amount_of_cells_revealed # Keep track of number of cells revealed
 
     # Calculate grid indices for the selected cell
     cell_row = cell.y // CELL_SIZE
     cell_col = cell.x // CELL_SIZE
 
+    # Count amount of cells revealed
+    amount_of_cells_revealed += 1
+
     # Check for bombs
     if cell.neighbouring_bombs == 0:
-        reveal_neighboring_cells(cell_row, cell_col)
+        reveal_neighbors(cell_row, cell_col)
 
 
-def reveal_neighboring_cells(row, col):
+def reveal_neighbors(row, col):
+    global amount_of_cells_revealed # Keep track of number of cells revealed
     for i in range(-1, 2):
         for j in range(-1, 2):
             new_row, new_col = row + i, col + j
@@ -142,10 +168,11 @@ def reveal_neighboring_cells(row, col):
                 # Check if the cell is not already selected to avoid infinite recursion
                 if not cell_around.selected:
                     cell_around.selected = True
+                    amount_of_cells_revealed += 1
 
                     # If the cell has no neighboring bombs, recursively reveal its neighbors
                     if cell_around.neighbouring_bombs == 0:
-                        reveal_neighboring_cells(new_row, new_col)
+                        reveal_neighbors(new_row, new_col)
 
 
 
@@ -191,6 +218,8 @@ def main():
     frame = 0
 
     while running:
+        win = False
+        global amount_of_cells_revealed, cells_in_game, amount_of_bombs
         screen.fill((0, 0, 0))
 
         for event in pygame.event.get():
@@ -198,12 +227,26 @@ def main():
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 left_mouse_button_click(event.pos)
+                #print(str(amount_of_cells_revealed) + " revealed")
+                #print(str(amount_of_cells) + " cells")
+                #print(str(amount_of_bombs) + " boms")
+                #print(amount_of_cells - amount_of_bombs)
+                if(amount_of_cells_revealed == cells_in_game - amount_of_bombs):
+                    win = True
 
         draw_cells()
         pygame.display.flip()
 
         clock.tick(30)
         frame += 1
+
+        if win:
+            counter = 0
+            while(counter < 25):
+                print("--------------------------------------------------------")
+                print("------------C0NGRATULATI0NS, Y0U WIN!!!!----------------")
+                counter += 1
+            reset_game()
 
     terminate_program()
 
